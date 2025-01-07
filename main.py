f7ccfa68249174b2012b7a8eb29d11e6895f3a7e
Douxy <douxy.pharma@gmail.com>
2025-01-07 18:06:17 +0100

    C2L3

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return    
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        print(f"dt={dt}")
 
         
 
