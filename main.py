87e1ab41c97089ceac43a119d0730d4d91f6e9bf
Douxy <douxy.pharma@gmail.com>
2025-01-07 19:49:33 +0100

    C2L7

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import *
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -12,7 +14,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,13 +29,21 @@ def main():
         dt = clock.tick(60)/1000
         #print(f"dt={dt}")
         pygame.Surface.fill(screen, "black")
-        player.update(dt)
-        player.draw(screen)
+
+        for thing in updatable:
+            thing.update(dt)
+
+        for thing in drawable:
+            thing.draw(screen)
+        
+        #player.update(dt)
+        #player.draw(screen)
         pygame.display.flip()
         
         
 
         
+
 
 
 
