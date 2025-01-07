22137032af76a8d1320373815bf08683560c63cf
Douxy <douxy.pharma@gmail.com>
2025-01-07 18:41:32 +0100

    C2L6

--- main.py
+++ main.py
@@ -17,11 +17,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return    
-        pygame.Surface.fill(screen, "black")
-        player.draw(screen)
-        pygame.display.flip()
         dt = clock.tick(60)/1000
         #print(f"dt={dt}")
+        pygame.Surface.fill(screen, "black")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+        
         
 
         
